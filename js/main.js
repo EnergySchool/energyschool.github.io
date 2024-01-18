44d7c5d3df355b959aca52071eeb6f988f2d176e
EnergySchool <135530291+EnergySchool@users.noreply.github.com>
2024-01-18 19:53:22 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
 "use strict";
 let  menuBtn=document.querySelector(".menu-btn-container");
 let menu=document.querySelector(".menu")
+let allCourse=document.querySelector(".allCourse");
+let courses=document.querySelectorAll(".course");
+
 menuBtn.addEventListener("click",function(){
    menu.classList.toggle("show")
 })
-
+if(window.innerWidth<600){
+   for(let i=4;i<courses.length;i++){
+      courses[i].classList.add("hidden")
+   }
+}
+allCourse.addEventListener("click",function(){
+   for(let i=4;i<courses.length;i++){
+      courses[i].classList.remove("hidden")
+   }
+})
 
 
 
